eb36e0a96e62f940aec32cdab7979da37e9937ec
Edward <edward.kallstedt@gmail.com>
2019-10-09 19:00:47 +0200

    fixed variable names

--- relay.py
+++ relay.py
@@ -1,63 +1,53 @@
 import RPi.GPIO as gpio
 import time
 
+PIN1 = 4
+PIN2 = 17
+PIN3 = 27
+PINPIN1 = 22
 
 def initGPIO():
     gpio.setmode(gpio.BCM)
 
-    gpio.setup(4, gpio.OUT)
-    gpio.setup(17, gpio.OUT)
-    gpio.setup(27, gpio.OUT)
-    gpio.setup(22, gpio.OUT)
+    gpio.setup(PIN1, gpio.OUT)
+    gpio.setup(PIN2, gpio.OUT)
+    gpio.setup(PIN3, gpio.OUT)
+    gpio.setup(PIN4, gpio.OUT)
     print("hej blabla")
 
 
 def turnOn(n):
     if(n==0):
-        gpio.output(4, gpio.LOW)
+        gpio.output(PIN1, gpio.LOW)
     elif (n==1):
-        gpio.output(17, gpio.LOW)
+        gpio.output(PIN2, gpio.LOW)
     elif (n==2):
-        gpio.output(27, gpio.LOW)
+        gpio.output(PIN3, gpio.LOW)
     elif (n==3):
-        gpio.output(22, gpio.LOW)
+        gpio.output(PIN4, gpio.LOW)
 
 def turnAllOn():
-    gpio.output(4, gpio.LOW)
-    gpio.output(17, gpio.LOW)
-    gpio.output(27, gpio.LOW)
-    gpio.output(22, gpio.LOW)
+    gpio.output(PIN1, gpio.LOW)
+    gpio.output(PIN2, gpio.LOW)
+    gpio.output(PIN3, gpio.LOW)
+    gpio.output(PIN4, gpio.LOW)
     
 def turnOff(n):
     if(n==0):
-        gpio.output(4, gpio.HIGH)
+        gpio.output(PIN1, gpio.HIGH)
     elif (n==1):
-        gpio.output(17, gpio.HIGH)
+        gpio.output(PIN2, gpio.HIGH)
     elif (n==2):
-        gpio.output(27, gpio.HIGH)
+        gpio.output(PIN3, gpio.HIGH)
     elif (n==3):
-        gpio.output(22, gpio.HIGH)
+        gpio.output(PIN4, gpio.HIGH)
 
 
 def turnAllOff():
-    gpio.output(4, gpio.HIGH)
-    gpio.output(17, gpio.HIGH)
-    gpio.output(27, gpio.HIGH)
-    gpio.output(22, gpio.HIGH)
+    gpio.output(PIN1, gpio.HIGH)
+    gpio.output(PIN2, gpio.HIGH)
+    gpio.output(PIN3, gpio.HIGH)
+    gpio.output(PIN4, gpio.HIGH)
 
 def cleanup():
     gpio.cleanup()
-#initGPIO()
-
-#i= 0
-#stop = 0
-
-#try:
-#    while(stop < 10):
-#        turnOn(i)
-#        time.sleep(2)
-#        turnOff(i)
-#        i = (i+1) % 4
-#        stop += 1
-#finally:
-#    gpio.cleanup()
